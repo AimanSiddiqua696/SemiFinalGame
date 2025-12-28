ff865d8754ddb0e19a7c7067388d09300149d2ca
Aiman Siddiqua <siddiquaaimen@gmail.com>
2025-12-28 10:52:55 +0500

    Update names of the sprite

--- SemiFinalGame/GameForm.Designer.cs
+++ SemiFinalGame/GameForm.Designer.cs
@@ -30,15 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
             tiles2 = new PictureBox();
-            tiles1 = new PictureBox();
-            box1 = new PictureBox();
-            box2 = new PictureBox();
             playerdown = new PictureBox();
+            box2 = new PictureBox();
+            box1 = new PictureBox();
+            tiles1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)tiles2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)tiles1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)box1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)box2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)playerdown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)box2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)box1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tiles1).BeginInit();
             SuspendLayout();
             // 
             // tiles2
@@ -46,20 +46,33 @@
             tiles2.Image = Properties.Resources.platform_tilesblue;
             tiles2.Location = new Point(-1, 544);
             tiles2.Name = "tiles2";
-            tiles2.Size = new Size(677, 69);
+            tiles2.Size = new Size(1014, 69);
             tiles2.SizeMode = PictureBoxSizeMode.StretchImage;
             tiles2.TabIndex = 1;
             tiles2.TabStop = false;
             // 
-            // tiles1
+            // playerdown
             // 
-            tiles1.Image = Properties.Resources.platform_tilesblue;
-            tiles1.Location = new Point(-1, 3);
-            tiles1.Name = "tiles1";
-            tiles1.Size = new Size(677, 67);
-            tiles1.SizeMode = PictureBoxSizeMode.StretchImage;
-            tiles1.TabIndex = 2;
-            tiles1.TabStop = false;
+            playerdown.BackColor = Color.Transparent;
+            playerdown.BackgroundImageLayout = ImageLayout.Stretch;
+            playerdown.Image = Properties.Resources.run_down0;
+            playerdown.Location = new Point(23, 230);
+            playerdown.Name = "playerdown";
+            playerdown.Size = new Size(99, 127);
+            playerdown.SizeMode = PictureBoxSizeMode.StretchImage;
+            playerdown.TabIndex = 5;
+            playerdown.TabStop = false;
+            playerdown.Tag = "obstacle";
+            // 
+            // box2
+            // 
+            box2.Image = Properties.Resources.box;
+            box2.Location = new Point(518, 393);
+            box2.Name = "box2";
+            box2.Size = new Size(76, 154);
+            box2.TabIndex = 4;
+            box2.TabStop = false;
+            box2.Tag = "obstacle";
             // 
             // box1
             // 
@@ -69,54 +82,47 @@
             box1.Size = new Size(77, 157);
             box1.TabIndex = 3;
             box1.TabStop = false;
+            box1.Tag = "obstacle";
             // 
-            // box2
+            // tiles1
             // 
-            box2.Image = Properties.Resources.box;
-            box2.Location = new Point(426, 395);
-            box2.Name = "box2";
-            box2.Size = new Size(76, 154);
-            box2.TabIndex = 4;
-            box2.TabStop = false;
-            // 
-            // playerdown
-            // 
-            playerdown.BackColor = Color.Transparent;
-            playerdown.Image = Properties.Resources.run_down0;
-            playerdown.Location = new Point(23, 199);
-            playerdown.Name = "playerdown";
-            playerdown.Size = new Size(127, 134);
-            playerdown.TabIndex = 5;
-            playerdown.TabStop = false;
+            tiles1.Image = Properties.Resources.platform_tilesblue;
+            tiles1.Location = new Point(-1, 0);
+            tiles1.Name = "tiles1";
+            tiles1.Size = new Size(1014, 75);
+            tiles1.SizeMode = PictureBoxSizeMode.StretchImage;
+            tiles1.TabIndex = 6;
+            tiles1.TabStop = false;
             // 
             // GameForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(650, 603);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(968, 603);
+            Controls.Add(tiles1);
             Controls.Add(playerdown);
             Controls.Add(box2);
             Controls.Add(box1);
-            Controls.Add(tiles1);
             Controls.Add(tiles2);
             Name = "GameForm";
             Text = "Game Form";
             Load += GameForm_Load;
             ((System.ComponentModel.ISupportInitialize)tiles2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)tiles1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)box1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)box2).EndInit();
             ((System.ComponentModel.ISupportInitialize)playerdown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)box2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)box1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tiles1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox tiles2;
-        private PictureBox tiles1;
-        private PictureBox box1;
-        private PictureBox box2;
         private PictureBox playerdown;
+        private PictureBox box2;
+        private PictureBox box1;
+        private PictureBox tiles1;
     }
 }
